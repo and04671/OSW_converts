dcba66aa636d46e3a85a613216feca4c3c0eefbd
Cole Anderson <cole55013@yahoo.com>
2024-02-01 16:18:19 -0800

    Script only updates first 230 ftrs

--- Montgomery MPN.py
+++ Montgomery MPN.py
@@ -1,9 +1,9 @@
 mc=iface.mapCanvas()
 layer = mc.currentLayer()
 dpr = layer.dataProvider()
-pfx = 'MPN_'
 
 def addPfx(layer, pfx):
+#a function to add a prefix to all original fields
     layer.startEditing()
     for field in layer.fields():
         oldname = field.name()
@@ -15,23 +15,26 @@ def addPfx(layer, pfx):
     layer.commitChanges()
 
 def addFld(layer, fldname, type):
+#a function to add a string or integer field to the selected layer
     dpr = layer.dataProvider()
     if type == "string":
         dpr.addAttributes([QgsField(fldname, QVariant.String)])
     if type == "integer":
         dpr.addAttributes([QgsField(fldname, QVariant.Int)])
     layer.updateFields()
-#comment
-
-
 
 
 for feature in layer.getFeatures():
     new_name = {feature.fieldNameIndex('highway'): 'footway'}
     dpr.changeAttributeValues({feature.id(): new_name})
+    print(feature.id())
+print("Complete")
+# with edit(layer):
+#     for f in layer.getFeatures():
+#         f['footway'] = f['highway']
+#         layer.updateFeature(f)    
 
 # layer.startEditing()
-
 # for feature in layer.getFeatures():
 #     value = expression.evaluate(feature)
 #     layer.changeAttributeValue(feature.id(), index, value)
@@ -43,9 +46,11 @@ for feature in layer.getFeatures():
 #     feature[new_field_index] = value
 #     layer.updateFeature(feature)
 
-#addPfx(layer, pfx)
-#addFld(layer, "highway","string")
-#addFld(layer, "footway","string")
+
+
+#addPfx(layer, 'MPN_')
+# addFld(layer, "highway","string")
+# addFld(layer, "footway","string")
 # addFld(layer, "width","integer")
 # addFld(layer, "crossing:markings","string")
 
